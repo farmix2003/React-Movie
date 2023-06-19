d86af3f6efdef9014970eec945c8dc3f6c6a7238
Farmix2003 <ffarrux386@gmail.com>
2023-06-19 07:36:21 +0300

    Finishing Project

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,13 +1,27 @@
 import Movie from "./Movie";
 
 const Movies = (props) => {
-  const { movies } = props;
+  const { movies = [] } = props;
 
   return (
     <div className="movies">
-      {movies.map((movie) => (
-        <Movie key={movie.imdbId} {...movie} />
-      ))}
+      {movies.length ? (
+        movies.map((movie) => <Movie key={movie.imdbId} {...movie} />)
+      ) : (
+        <div className="not-found">
+          <h1>404</h1>
+
+          <div class="frame">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div class="caps">
+            <img src="http://ademilter.com/caps.png" alt="" />
+          </div>
+          <canvas id="canvas"></canvas>
+        </div>
+      )}
     </div>
   );
 };
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -5,30 +5,32 @@ import Search from "../components/Search";
 export default class Main extends React.Component {
   state = {
     movies: [],
+    loading: true,
   };
   componentDidMount() {
     fetch("http://www.omdbapi.com/?apikey=b8202e0a&s=panda")
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
   }
 
   SearchMovies = (str, type = "all") => {
+    this.setState({ loading: true });
     fetch(
       `http://www.omdbapi.com/?apikey=b8202e0a&s=${str}${
         type !== "all" ? `&type = ${type}` : ""
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
   };
   render() {
     return (
       <div className="container content">
         <Search SearchMovies={this.SearchMovies} />
-        {this.state.movies.length ? (
-          <Movies movies={this.state.movies} />
-        ) : (
+        {this.state.loading ? (
           <Loader />
+        ) : (
+          <Movies movies={this.state.movies} />
         )}
       </div>
     );
